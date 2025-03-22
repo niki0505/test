a3cfb18c86e3235ea9d5f3e945cb8dbb8f60c36e
niki0505 <bataclanknicole2004@gmail.com>
2025-03-22 10:23:21 +0800

    App.js updated

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello I'm Niki!</Text>
+      <Text>Hi I'm Niki!</Text>
       <StatusBar style="auto" />
     </View>
   );
